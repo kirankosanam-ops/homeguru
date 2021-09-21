21a90da2f5dcebf08167dd7b3ae46d6486440290
kirankosanam-ops <72398006+kirankosanam-ops@users.noreply.github.com>
2021-09-21 18:21:24 +0530

    login api completed

--- api.js
+++ api.js
@@ -1,6 +1,5 @@
 // requiring the packages
-// const db = require('./database/dboperations');
-
+const db = require('./database/dboperations');
 const users = require('./database/users');
 const {log} = require("nodemon/lib/utils");
 const bodyParser = require('body-parser');
@@ -12,44 +11,59 @@ const config = require("./database/dbconfig");
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '\\views\\signup.html');
+//lOGIN PAGE
+app.get('/login', function (req, res) {
+    res.sendFile(__dirname + '\\views\\login.html');
 
 });
+app.post('/login', function (req, res) {
 
-app.post('/',  function (req, res) {
-    res.send("Thanks for posting");
 
     const email = req.body.email;
     const password = req.body.password;
-    const username = req.body.username;
-
-    const x = async function isRegisteredUser() {
-        console.log("hello");
-        try {
-            let pool = await sql.connect(config);
-            let users = await pool.request().query("SELECT EMAIL FROM USERS WHERE EMAIL LIKE " + "'%" + email + "%'");
-
-            //if user didn't exist in the users database return true else false
-            // if( users.recordsets.toString() === ''){
-                console.log("db_response");
-                let db_response = await pool.request().query("INSERT INTO USERS VALUES (" + username + ", " + email + ", " + password + ")");
-                console.log(db_response);
-
-        } catch (error) {
-            throw error;
-        }
+    if(email === '' || password === '' ){
+        res.post('email and password are required to login');
     }
-    console.log(x);
 
+
+    db.isRegUser(email, password).then(result => {
+        console.log("in" + result);
+        if(result){
+            res.send("login failed, user not found");
+        } else {
+            res.send("login successful");
+        }
+    });
+
+});
+
+//SIGNUP PAGE
+app.get('/signup', function (req, res) {
+    res.sendFile(__dirname + '\\views\\signup.html');
+
+});
+app.post('/signup', function (req, res) {
+    const email = req.body.email;
+    const password = req.body.password;
+    const username = req.body.username;
+    if(email === '' || password === '' || username === '' ){
+        res.post('username email and password are required to login');
+    }
+
+
+    db.isRegUser(email, password).then(result => {
+
+        if(result){
+            db.createUser(username, email, password).then(result => {
+                console.log(result);
+            });
+        } else {
+            res.send("Creation of new user failed, user found in database");
+        }
+    });
 
 });
 
 app.listen(3000, function () {
     console.log('Server is running');
 });
-
-// let isNewUser = db.getUsers().then(result => {
-//     console.log("is new user " + result);
-//     return result;
-// });
--- database/dboperations.js
+++ database/dboperations.js
@@ -2,22 +2,31 @@ const config = require('./dbconfig');
 const sql = require('mssql');
 
 
-async function isRegisteredUser(){
-    try{
+async function isRegisteredUser(email, password) {
+    try {
         let pool = await sql.connect(config);
+        let users = await pool.request().query("SELECT EMAIL, PASSWORD FROM USERS WHERE EMAIL LIKE " + "'%" + email + "%' AND PASSWORD LIKE " + "'%" + password + "%'");
+        return users.recordsets.toString() === '';
+    } catch (error) {
+        throw error;
+    }
+}
 
-        let users = await pool.request().query("SELECT EMAIL FROM USERS WHERE EMAIL LIKE "+"'%"+cccc+"%'");
-        if (users.recordsets.toString() === ''){
-            // console.log('user not found');
-            return false;
-        }
-        return true;
-
-    } catch (error){
-        console.log(error);
+async function createNewUser(username, email, password) {
+    let insertQuery = "INSERT INTO USERS (USERNAME, EMAIL, PASSWORD) VALUES ("+ username.toString() + ", " + email.toString() + ", " + password.toString() + ")";
+    try {
+        let pool = await sql.connect(config);
+        await pool.request().query(insertQuery, function(err, result){
+            if (err) throw err;
+            console.log("1 record inserted");
+            return "1 record inserted";
+        });
+    } catch (error) {
+        throw error;
     }
 }
 
 module.exports = {
-    getUsers : isRegisteredUser
+    isRegUser : isRegisteredUser,
+    createUser : createNewUser
 }
